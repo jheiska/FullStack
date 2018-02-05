ba64c3fd9e939d4ea35113938c4888a94077f989
Jaakko Heiska <jaakkoheiska@gmail.com>
2018-02-05 15:29:37 +0200

    2.6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,21 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  addPerson = (event) => {
+    event.preventDefault()
+    const person = {
+      name: this.state.newName
+    }   
+
+    const persons = this.state.persons.concat(person)
+
+    this.setState({
+      persons: persons,
+      newName: ''
+    })
+
+    }
+
+    handleNameChange = (event) => {
+      console.log(event.target.value)
+      this.setState({ newName: event.target.value })
+    } 
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div>
+          debug: {this.state.newName}
+        </div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addPerson}>
+          <div>
+            nimi:
+          </div>
+          <div>
+          <input 
+            value={this.state.newName}
+            onChange={this.handleNameChange} 
+          />
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        {this.state.persons.map(person => <li key={person.name}>{person.name}</li>)}
+
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
