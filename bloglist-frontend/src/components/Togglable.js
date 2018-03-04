9c019b6cf3a3a3c84c3bf50d3e6ff7eba0a813ab
Jaakko Heiska <jaakkoheiska@gmail.com>
2018-03-04 14:59:21 +0200

    5.11

--- bloglist-frontend/src/components/Togglable.js
+++ bloglist-frontend/src/components/Togglable.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class Togglable extends React.Component {
   constructor(props) {
@@ -7,6 +8,7 @@ class Togglable extends React.Component {
       visible: false
     }
   }
+
 
   toggleVisibility = () => {
     this.setState({visible: !this.state.visible})
@@ -29,5 +31,10 @@ class Togglable extends React.Component {
     )
   }
 }
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+}
+
 
 export default Togglable
